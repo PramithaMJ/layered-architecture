0c28eae8ef3996c8655ed88e0c7ddc6b6b43096a
Pramitha Jayasooriya <lpramithamj@gmail.com>
2025-03-09 06:44:58 +0530

    user request

--- src/main/java/com/pmj/template/dto/request/UserRequest.java
+++ src/main/java/com/pmj/template/dto/request/UserRequest.java
@@ -1,2 +1,34 @@
-package com.pmj.template.dto.request;public class UserRequest {
+package com.pmj.template.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, max = 120, message = "Password must be between 6 and 120 characters")
+    private String password;
+
+    private String fullName;
+
+    private Set<String> roles;
 }
