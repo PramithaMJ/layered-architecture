96dd9e4af306d313c6009e0b2664c67ecbe602cc
Pramitha Jayasooriya <lpramithamj@gmail.com>
2025-03-09 06:45:46 +0530

    user response

---
+++ src/main/java/com/pmj/template/dto/response/UserResponse.java
@@ -0,0 +1,25 @@
+package com.pmj.template.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String fullName;
+    private Set<String> roles;
+    private boolean enabled;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
